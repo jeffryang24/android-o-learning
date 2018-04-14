5f5e5695724d364c79f88bac12d5c669fd781c38
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-04-14 22:10:14 +0700

    Interacting using editbox

--- ExampleApp_2_6/app/src/main/java/com/myappcompany/rob/exampleapp/MainActivity.java
+++ ExampleApp_2_6/app/src/main/java/com/myappcompany/rob/exampleapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFunction(View view) {
+        EditText txtEmailAddr = findViewById(R.id.txtEmailAddr);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        String email = txtEmailAddr.getText().toString();
+        String password = txtPassword.getText().toString();
+        String status = "";
+
+        if (email.equals("admin@localhost.dev") && password.equals("admin")) {
+            status = "Login Success!";
+        } else {
+            status = "Login Failed!";
+        }
+
         Log.i("Info", "Button pressed!");
+        Log.i("status", status);
     }
 }
