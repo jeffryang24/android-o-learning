e899c59f6c95873b20a269464ce5930f33992adc
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-04-15 12:06:44 +0700

    Fix inflation error while testing using hardware

--- Image/app/src/main/java/com/example/jeffryangtoni/image/MainActivity.java
+++ Image/app/src/main/java/com/example/jeffryangtoni/image/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     int i = 1;
